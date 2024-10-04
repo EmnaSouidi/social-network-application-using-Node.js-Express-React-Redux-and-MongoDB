29680dcfc6ce1621dd4c69165f95e83ce61354c6
Emna Souidi <emna.jlassi80@gmail.com>
2024-10-04 14:27:53 +0100

    Impelementing react

--- server.js
+++ server.js
@@ -45,5 +45,5 @@ app.use('/api/posts',posts);
 
 // app.listen(3000, ()=> console.log('Server is running on port 3000'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, ()=>console.log(`server is running on ${port}`));
