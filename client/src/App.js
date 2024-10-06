be371c9964c172277c5bf91a8ae063203829b8a2
Emna Souidi <emna.jlassi80@gmail.com>
2024-10-06 17:56:19 +0100

    Create Layout /component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
+import React, {Component} from 'react';
 
-import './App.css';
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <h1>My react App</h1>
     </div>
   );
+}
 }
 
 export default App;
